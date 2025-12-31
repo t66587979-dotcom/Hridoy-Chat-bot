394cd3b17f7dce2d4475a9111b67dd9146f121b4
hridoyhossen48360 <alaminkhan123466hh@gmail.com>
2026-01-01 02:20:41 +0600

    Add files via upload

--- Script/commands/random.js
+++ Script/commands/random.js
@@ -1,36 +1,42 @@
 module.exports.config = {
- 'name': "random",
- 'version': "11.9.7",
- 'hasPermission': 0,
- 'credits': "Shaon Ahmed",
- 'description': "random love story video",
- 'commandCategory': "video",
- 'usages': "random",
- 'cooldowns': 5
+  name: "random",
+  version: "11.9.7",
+  hasPermssion: 0,
+  credits: "Shaon Ahmed",
+  description: "random love story video",
+  usePrefix: true,
+  commandCategory: "video",
+  usages: "random",
+  cooldowns: 30,
 };
 
-module.exports.run = async function ({ api, event }) {
- const axios = require("axios");
- const request = require("request");
- const fs = require('fs');
- 
- const apiResponse = await axios.get("https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json");
- const apiUrl = apiResponse.data.api;
- 
- var videoUrls = [apiUrl + "/video/random"];
- var randomUrl = videoUrls[Math.floor(Math.random() * videoUrls.length)];
- 
- axios.get(randomUrl).then(response => {
- let videoCount = response.data.count;
- let videoName = response.data.name;
- 
- let sendVideo = function () {
- api.sendMessage({
- 'body': "𝐒𝐏𝐀𝐘𝐒𝐇𝐄𝐀𝐋 𝐑𝐀𝐍𝐃𝐎𝐌 𝐌𝐈𝐗 \nAdded by: [" + videoName + "]\n𝚃𝙾𝚃𝙰𝙻 𝚅𝙸𝙳𝙴𝙾:" + videoCount + "...🎬\n\n｢─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭｣",
- 'attachment': fs.createReadStream(__dirname + "/cache/Shaoon.mp4")
- }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/Shaoon.mp4"), event.messageID);
- };
- 
- request(response.data.url).pipe(fs.createWriteStream(__dirname + "/cache/Shaoon.mp4")).on("close", sendVideo);
- });
-};
+module.exports.run = async function({ api, event, args }) {
+    const axios = require("axios")
+    const request = require("request")
+    const fs = require("fs-extra")
+  const apis = await axios.get('https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json')
+  const n = apis.data.api;
+    const res = await axios.get(`${n}/video/random`);
+    var data = res.data.url;
+    var msg = [];
+    let video = `${res.data.url}`;
+  let name = `${res.data.name}`;
+    let cp = `${res.data.cp}`
+  let ln = `${res.data.count}`
+
+    let videos = (await axios.get(`${video}`, {
+        responseType: 'arraybuffer'
+    })).data;
+    fs.writeFileSync(__dirname + "/cache/video.mp4", Buffer.from(videos, "utf-8"));
+    var allimage = [];
+    allimage.push(fs.createReadStream(__dirname + "/cache/video.mp4"));
+
+    {
+        msg += `${cp}\n\n𝐓𝐨𝐭𝐚𝐥 𝐕𝐢𝐝𝐞𝐨𝐬: [${ln}]\n𝐀𝐝𝐝𝐞𝐝 𝐓𝐡𝐢𝐬 𝐕𝐢𝐝𝐞𝐨 𝐓𝐨 𝐓𝐡𝐞 𝐀𝐩𝐢 𝐁𝐲 [${name}]`
+    }
+
+    return api.sendMessage({
+        body: msg,
+        attachment: allimage
+    }, event.threadID, event.messageID);
+}
