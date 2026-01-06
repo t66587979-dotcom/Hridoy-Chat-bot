8333ed48c5e747201f77919f5b9e92b47a1c7336
t66587979-dotcom <t66587979@gmail.com>
2026-01-06 18:00:24 +0600

    Update fork.js

--- Script/commands/fork.js
+++ Script/commands/fork.js
@@ -4,7 +4,7 @@ version: "1.0.0",
 hasPermssion: 0,
 credits: "SHAHADAT SAHU",
 description: "Send GitHub repo link",
-commandCategory: "tool",
+commandCategory: "Tool",
 usages: "fork",
 cooldowns: 3,
 };
