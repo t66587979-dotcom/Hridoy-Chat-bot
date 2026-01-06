862ecc63f677a5b4ca0a150668d767e438d8278c
t66587979-dotcom <t66587979@gmail.com>
2026-01-06 18:25:23 +0600

    Update sendsms.js

--- Script/commands/sendsms.js
+++ Script/commands/sendsms.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	hasPermssion: 2,
 	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Send messages to groups!",
-	commandCategory: "system",
+	commandCategory: "System",
 	usages: "[Text]",
 	cooldowns: 5
 };
